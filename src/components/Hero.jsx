bb353868272b51b4632897a0e1b4e8cc98354ca4
k70118088-star <k70118088@gmail.com>
2026-03-30 14:04:17 +0530

    added correct margin in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,12 +10,12 @@ const Hero = () => {
         <h1 className="font-semibold max-w-182 text-center text-3xl sm:text-5xl lg:text-6xl xl:text-7xl leading-[130%] bg-gradient-to-r from-black to-[#494949] bg-clip-text text-transparent">
           A wonderful way to Transform your walls.
         </h1>
-        <p className="mt-4 sm:mt-6 max-w-170 text-center font-medium text-sm lg:text-lg xl:text-xl leading-[140%] text-black">
+        <p className="mt-4 sm:mt-6.25 max-w-170 text-center font-medium text-sm lg:text-lg xl:text-xl leading-[140%] text-black">
           No matter your decorating style, we've got the tricks that can give life to your
           walls and bring out your true personality.
         </p>
         <Button
-          className="mt-5 sm:mt-6 flex items-center py-2 sm:py-3 px-5 sm:px-[24.5px] font-semibold text-sm sm:text-lg lg:text-xl text-white rounded-[82px] bg-[#ED1C25] shadow-[0px_12px_43.8px_-10px_#ED1C25]"
+          className="mt-5 sm:mt-6.25 flex items-center py-2 sm:py-3 px-5 sm:px-[24.5px] font-semibold text-sm sm:text-lg lg:text-xl text-white rounded-[82px] bg-[#ED1C25] shadow-[0px_12px_43.8px_-10px_#ED1C25]"
           text="Square Your Photos"
         />
         <p className="mt-12 sm:mt-20 lg:mt-22.25 font-normal text-[12px] leading-[100%] text-[#1E1E1E]">
